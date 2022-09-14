0baa43809746638ecc87acfc7ec8a7d7934edf3d
JeongAnNa <rib040211@gmail.com>
2022-09-14 20:55:16 +0900

    [Update] Responding to AZ on index request

--- app-a/routes/index.js
+++ app-a/routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
+require('dotenv').config();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { az: process.env.AZ });
 });
 
 module.exports = router;
